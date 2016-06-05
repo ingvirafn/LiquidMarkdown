fc50dffcb027d1b0b349dcef37969d8f46c23cf4
Ingvi Rafn <ingvirafn@ingvirafn.com>
2016-06-05 13:22:06 +0000

    Added Liquid processor

---
+++ src/Markdown.Xaml/MarkdownLiquid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace Markdown.Xaml
+{
+    public class MarkdownLiquid : Markdown
+    {
+        public override FlowDocument Transform(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+                return base.Transform(text);
+            try
+            {
+
+                var tmp = DotLiquid.Template.Parse(text);
+                var rdr = tmp.Render();
+                return base.Transform(rdr);
+            }
+            catch (Exception e)
+            {
+                return base.Transform(text);
+            }
+
+        }
+    }
+}
